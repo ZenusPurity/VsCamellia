1955dd663b7df08c9798734a573e87137873c620
ZenusPurity <zachary0502.ramos@gmail.com>
2021-07-17 14:09:42 +0800

    Seizure Warning

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -30,25 +30,24 @@ class OptionsMenu extends MusicBeatState
 	var options:Array<OptionCategory> = [
 		new OptionCategory("C-Mode", [
 			new CMode("Play the harder charts as Camellia!"),
+			#if windows
 			new Modcharts("Throw special effects in the mix! !!SEIZURE WARNING!!"),
+			#end
 			new HealthDrain("Drains health in C-Mode")
 			// new DamageMode("Adds a chance for damage notes to appear. It's random! CURRENTLY IN TESTING, you have been warned!")
 		]),
 		new OptionCategory("Custom Features",[
 			new HitNoise("Plays the hit noise from other 4 key games"),
 			#if windows
-			new Modcharts("Throw special effects in the mix!"),
-			new Vocals("Toggle the vocals playing on songs.")
+			new Modcharts("Throw special effects in the mix! !!SEIZURE WARNING!!"),
 			#end
+			new Vocals("Toggle the vocals playing on songs.")
 		]),
 		new OptionCategory("Gameplay", [
 			new DFJKOption(controls),
 			new DownscrollOption("Change the layout of the strumline."),
 			new GhostTapOption("Ghost Tapping is when you tap a direction and it doesn't give you a miss."),
 			new Judgement("Customize your Hit Timings (LEFT or RIGHT)"),
-			#if desktop
-			new FPSCapOption("Cap your FPS"),
-			#end
 			new ScrollSpeedOption("Change your scroll speed (1 = Chart dependent)"),
 			new AccuracyDOption("Change how accuracy is calculated. (Accurate = Simple, Complex = Milisecond Based)"),
 			new ResetButtonOption("Toggle pressing R to gameover."),
@@ -70,14 +69,12 @@ class OptionsMenu extends MusicBeatState
 		
 		new OptionCategory("Misc", [
 			#if desktop
-			new FPSOption("Toggle the FPS Counter"),
 			new ReplayOption("View replays"),
 			#end
 			new FlashingLightsOption("Toggle flashing lights that can cause epileptic seizures and strain."),
 			new WatermarkOption("Enable and disable all watermarks from the engine."),
 			new ScoreScreen("Show the score screen after the end of a song"),
 			new ShowInput("Display every single input in the score screen."),
-			new Optimization("No backgrounds, no characters, centered notes, no player 2."),
 			new BotPlay("Showcase your charts and mods with autoplay."),
 		])
 		
